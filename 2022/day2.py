09c3dc87dc72c74b4747af666ee17a9d5b407679
Micah Huff <mphuff@amazon.com>
2022-12-01 22:23:07 -0800

    Part 2 of day 2 complete

--- 2022/day2.py
+++ 2022/day2.py
@@ -8,16 +8,30 @@ class Shape(Enum):
     PAPER = 2
     SCISSORS = 3
 
+    # Specifies what shape this particular shape defeats as only one option is valid
+    def beats(self):
+        if self == Shape.ROCK:
+            return Shape.SCISSORS
+        elif self == Shape.PAPER:
+            return Shape.ROCK
+        elif self == Shape.SCISSORS:
+            return Shape.PAPER
+
+    # Specifies what shape this particular shape defeats as only one option is valid
+    def loses_to(self):
+        if self == Shape.SCISSORS:
+            return Shape.ROCK
+        elif self == Shape.ROCK:
+            return Shape.PAPER
+        elif self == Shape.PAPER:
+            return Shape.SCISSORS
+
     # Determines whether this shape beats the other one
     def defeats_other(self, other_shape):
         if self.name == other_shape.name:
             return Score.DRAW
 
-        if self == Shape.ROCK and other_shape == Shape.SCISSORS:
-            return Score.WIN
-        elif self == Shape.SCISSORS and other_shape == Shape.PAPER:
-            return Score.WIN
-        elif self == Shape.PAPER and other_shape == Shape.ROCK:
+        if self.beats() == other_shape:
             return Score.WIN
 
         return Score.LOSS
@@ -27,6 +41,16 @@ class Score(Enum):
     LOSS = 0
     DRAW = 3
     WIN = 6
+
+    def shape_from_desired_outcome(self, opponent_shape):
+        if self == Score.DRAW:
+            return opponent_shape
+        elif self == Score.LOSS:
+            # retrieve the shape that opponent shape defeats
+            return opponent_shape.beats()
+        elif self == Score.WIN:
+            # retrieve the shape that this shape loses_to
+            return opponent_shape.loses_to()
 
 
 class InputToShape(Enum):
@@ -38,28 +62,56 @@ class InputToShape(Enum):
     Z = Shape.SCISSORS
 
 
+class OutcomeToScore(Enum):
+    X = Score.LOSS
+    Y = Score.DRAW
+    Z = Score.WIN
+
+
+def part1(input_csv):
+    running_my_score = 0
+
+    with open(input_csv) as csvfile:
+        reader = csv.reader(csvfile, delimiter=' ')
+
+        for row in reader:
+            opponent_shape = InputToShape.__getitem__(row[0]).value
+            my_shape = InputToShape.__getitem__(row[1]).value
+
+            # outcome will be a Score object, returned by defeats_other method
+            outcome = my_shape.defeats_other(opponent_shape)
+
+            running_my_score += outcome.value + my_shape.value
+
+    print("Part 1 final score = %s" % running_my_score)
+
+
+def part2(input_csv):
+    running_my_score = 0
+
+    with open(input_csv) as csvfile:
+        reader = csv.reader(csvfile, delimiter=' ')
+
+        for row in reader:
+            opponent_shape = InputToShape.__getitem__(row[0]).value
+            desired_outcome = OutcomeToScore.__getitem__(row[1]).value
+
+            my_shape = desired_outcome.shape_from_desired_outcome(opponent_shape)
+
+            # outcome will be a Score object, returned by defeats_other method
+            outcome = my_shape.defeats_other(opponent_shape)
+
+            assert outcome == desired_outcome
+
+            running_my_score += outcome.value + my_shape.value
+
+    print("Part 2 final score = %s" % running_my_score)
+
+
 if __name__ == '__main__':
     if len(sys.argv) < 1:
         raise Exception("No file provided as a parameter")
 
-    running_computer_score = 0
-    running_my_score = 0
-
-    with open(sys.argv.pop()) as csvfile:
-        reader = csv.reader(csvfile, delimiter=' ')
-
-        for row in reader:
-            print(',,,,,'.join(row))
-            opponent_shape = InputToShape.__getitem__(row[0]).value
-            my_shape = InputToShape.__getitem__(row[1]).value
-            print(opponent_shape)
-            print(my_shape)
-
-            # outcome will be a Score object, returned by defeats_other method
-            outcome = my_shape.defeats_other(opponent_shape)
-            print("outcome = %s" % outcome)
-
-            score_this_round = outcome.value + my_shape.value
-            running_my_score += score_this_round
-
-    print("My final score = %s" % running_my_score)
+    input_file = sys.argv.pop()
+    part1(input_file)
+    part2(input_file)

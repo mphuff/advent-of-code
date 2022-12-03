4428d7b2adf2d5f224daa7e8902005f44d124033
Micah Huff <micah@micahhuff.com>
2022-12-02 22:09:43 -0800

    Day 3, part 2

--- 2022/day3.py
+++ 2022/day3.py
@@ -2,6 +2,7 @@ import csv
 import sys
 
 def calculate_priority(item):
+    # requirements
     # a-z = 1-26
     # A-Z = 27-52
 
@@ -9,6 +10,7 @@ def calculate_priority(item):
     # capital letter
     if ordinal < 96:
         return ordinal - 64 + 26
+    # lowercase letter
     else:
         return ordinal - 96
 
@@ -17,15 +19,19 @@ if __name__ == '__main__':
     if len(sys.argv) < 1:
         raise Exception("No file provided as a parameter")
 
-    total_priority = 0;
+    total_priority = 0
 
     with open(sys.argv.pop()) as csvfile:
         reader = csv.reader(csvfile)
 
+        p2s = []
+        priority_per_3_elves = 0
         for row in reader:
+
             total_items = list(row[0])
             assert len(total_items) % 2 == 0
 
+            # problem 1
             sack1 = total_items[:int((len(total_items)/2))]
             sack2 = total_items[int(len(total_items) / 2):]
 
@@ -36,6 +42,15 @@ if __name__ == '__main__':
 
             total_priority += priority
 
-            print("%s / %s" % (shared, priority))
+            # problem 2
+            p2s.append(list(row[0]))
 
-        print(total_priority)
+            if len(p2s) == 3:
+                for x in p2s[0]:
+                    if x in p2s[1] and x in p2s[2]:
+                        priority = calculate_priority(x)
+                priority_per_3_elves += priority
+                p2s = []
+
+        print("part 1 = %s" % total_priority)
+        print("part 2 = %s" % priority_per_3_elves)
